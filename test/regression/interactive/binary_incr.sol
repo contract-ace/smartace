b34015c1fc38b91343fc94da515c3d35f6cf7d74
ScottWe <scott.wesley@ns.sympatico.ca>
2019-10-15 16:28:31 -0400

    Moved over BinaryIncr test. This test exercises conditional transact bodies, parametric transactions with non-trivial side-effects, and linear constraints.

---
+++ test/regression/interactive/binary_incr.sol
@@ -0,0 +1,31 @@
+// Copyright 2017-2019 ConsenSys AG
+//
+// RUN: %solc %s --c-model --output-dir=%t
+// RUN: cd %t
+// RUN: cmake -DSEA_PATH=%seapath
+// RUN: make icmodel
+// RUN: echo 1 0 0 0 1 1 1 0 0 0 1 1 1 0 0 0 1 1 1 0 0 0 1 0 1 0 0 0 1 1 1 0 0 0 0 | ./icmodel --return-0 --count-transactions 2>&1 | OutputCheck %s --comment=//
+// CHECK: assert
+// CHECK: Transaction Count: 6
+
+/*
+ * Regression test to ensure we can emulate contracts with binary inputs, and
+ * integer output values.
+ */
+
+contract Foo {
+	int256 private x;
+
+	function Bar() public view returns (int256) {
+		if (x == 29) {
+			assert(false);
+			return 1;
+		}
+		return 0;
+	}
+
+	function SetNext(bool b) public {
+		x = 2*x + (b ? 1 : 0);
+	}
+}
+
---
+++ test/regression/verify/binary_incr.sol
@@ -0,0 +1,30 @@
+// Copyright 2017-2019 ConsenSys AG
+//
+// RUN: %solc %s --c-model --output-dir=%t
+// RUN: cd %t
+// RUN: cmake -DSEA_PATH=%seapath
+// RUN: make verify | OutputCheck %s --comment=//
+// CHECK: ^sat$
+
+/*
+ * Regression test to ensure we can verify transactional sequences in which many
+ * transaction are required to form a counter-example, and in which each such
+ * transaction is parametric.
+ */
+
+contract Foo {
+	int256 private x;
+
+	function Bar() public view returns (int256) {
+		if (x == 29) {
+			assert(false);
+			return 1;
+		}
+		return 0;
+	}
+
+	function SetNext(bool b) public {
+		x = 2*x + (b ? 1 : 0);
+	}
+}
+
