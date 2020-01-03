ddf3713a4b7098ffc89714b4c95df8f432ed2f15
scottwe <scott.wesley@ns.sympatico.ca>
2020-01-02 23:49:55 -0500

    Test case for specialization by upcast.

---
+++ test/regression/verify/specialize_cast.sol
@@ -0,0 +1,26 @@
+// RUN: %solc %s --c-model --output-dir=%t
+// RUN: cd %t
+// RUN: cmake -DSEA_PATH=%seapath
+// RUN: make verify 2>&1 | OutputCheck %s --comment=//
+// CHECK: unsat
+
+/**
+ * This model requires that DerivedContract is assignable to BaseContract. As
+ * the model specializes contracts, this requires knowledge that c is equivalent
+ * to DerivedContract.
+ */
+
+contract BaseContract {
+	int a;
+	constructor(int _a) public { a = _a; }
+}
+
+contract DerivedContract is BaseContract {
+	constructor() public BaseContract(5) {}
+	function g() public view returns (int) { return a; }
+}
+
+contract User {
+	BaseContract c;
+	constructor() public { c = new DerivedContract(); }
+}
